c4330b47360e708d8ff8ea7ee362ae192ddddf31
Gift Mbonani <giftmbonani@outlook.com>
2024-11-28 20:02:16 +0000

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""making Change"""
+
+
+
+def makeChange(coins, total):
+    """making Change"""
+    if total <= 0:
+        return 0
+    elif total > 0:
+        newList = sorted(coins[:])
+        newList = list(reversed(newList))
+        count = 0
+        value = total + 0
+        index = 0
+        while value >= 0 and (index < len(newList)):
+            if value >= newList[index]:
+                value = value - newList[index]
+                count += 1
+            elif value < newList[index]:
+                index += 1
+        if index == len(newList):
+            if value != 0:
+                return -1
+            elif value == 0:
+                return count
